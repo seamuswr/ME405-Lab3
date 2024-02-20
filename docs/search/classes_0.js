5cdbe669f0674d5020b06c2d949d3f41156f34ec
seamuswr <108034107+seamuswr@users.noreply.github.com>
2024-02-20 11:18:57 -0800

    Ran Doxygen

---
+++ docs/search/classes_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['closed_5floop_0',['closed_loop',['../classclosed__loop_1_1closed__loop.html',1,'closed_loop']]]
+];
---
+++ docs/search/functions_2.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['set_5fduty_5fcycle_0',['set_duty_cycle',['../class_motor_driver_1_1_motor_driver.html#a8fa27d276c15388a80f7c8a4a84b73ad',1,'MotorDriver::MotorDriver']]]
+];
